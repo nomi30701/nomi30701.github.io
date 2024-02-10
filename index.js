ffe0079f9afabf17309d620ac2b726332cb33b13
Nomi <84728127+nomi3070@users.noreply.github.com>
2024-02-10 20:14:01 +0800

    update

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-// Define a model for linear regression. The script tag makes `tf` available
-// as a global variable.
-const model = tf.sequential();
-model.add(tf.layers.dense({units: 1, inputShape: [1]}));
-
-model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
-
-// Generate some synthetic data for training.
-const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
-const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
-
-function tensorflow_random_btn(){
-    // Train the model using the data.
-    model.fit(xs, ys, {epochs: 10}).then(() => {
-        result = model.predict(tf.tensor2d([5], [1, 1]))
-        document.getElementById('tensorflow_test').textContent = `predict: ${result}`;
-    });
+function dropdown() {
+    const dropdown_menu = document.querySelector('.dropdown_menu');
+    const dropdown_box = document.querySelectorAll('.dropdown_menu .div')
+    const arrow = document.querySelector('.arrow-down');
+    dropdown_menu.classList.toggle('show'); // add or delete .block to dropdown_menu
+    arrow.classList.toggle('rotate'); // add or delete .rotate to arrow
 }
+
+// box animation
+const boxes = document.querySelectorAll('.box');
+function applyFadeInAnimation() {
+    for (var i = 0; i < boxes.length; i++) {
+        var delay = i * 50;
+        boxes[i].style.animation = `fadeIn 1s ease forwards ${delay}ms`;
+    }
+}
+
+applyFadeInAnimation();
