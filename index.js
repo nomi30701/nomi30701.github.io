f2868c78ec76fe4ffb7f03b72447bed0e691bb3a
Nomi <84728127+nomi30701@users.noreply.github.com>
2024-02-11 00:06:40 +0800

    fix animation

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
+// drop event
 function dropdown() {
     const dropdown_menu = document.querySelector('.dropdown_menu');
-    const dropdown_box = document.querySelectorAll('.dropdown_menu .div')
     const arrow = document.querySelector('.arrow-down');
-    dropdown_menu.classList.toggle('show'); // add or delete .block to dropdown_menu
-    arrow.classList.toggle('rotate'); // add or delete .rotate to arrow
+    const dropdown_boxes = dropdown_menu.querySelectorAll('.box');
+
+    dropdown_menu.classList.toggle('show');
+    arrow.classList.toggle('rotate');
+
+    if (dropdown_menu.classList.contains('show')) {
+        dropdown_boxes.forEach((box, index) => {
+            var delay = index * 100;
+            box.style.animation = `fadeIn 0.5s ease forwards ${delay}ms`;
+            box.style.visibility = 'visible';
+        });
+    } else {
+        dropdown_boxes.forEach(box => {
+            box.style.visibility = 'hidden';
+        });
+    }
 }
 
 // box animation
-// const boxes = document.querySelectorAll('.box');
-// function applyFadeInAnimation() {
-//     for (var i = 0; i < boxes.length; i++) {
-//         var delay = i * 50;
-//         boxes[i].style.animation = `fadeIn 1s ease forwards ${delay}ms`;
-//     }
-// }
+const boxes = document.querySelectorAll('.box');
+function applyFadeInAnimation() {
+    boxes.forEach((box, index) => {
+        var delay = index * 50;
+        box.style.animation = `fadeIn 0.5s ease forwards ${delay}ms`;
+    });
+}
 
-// applyFadeInAnimation();
+// apply box animation
+applyFadeInAnimation();
