426e953c55e354116ccfee20759dc6b0e7db4d15
Nomi <84728127+nomi30701@users.noreply.github.com>
2024-02-11 23:00:00 +0800

    fix animation

--- index.js
+++ index.js
@@ -2,6 +2,35 @@
 function dropdown() {
     const dropdown_menu = document.querySelector('.dropdown_menu');
     const arrow = document.querySelector('.arrow-down');
-    dropdown_menu.classList.toggle('show');
+    dropdown_menu.classList.toggle('show_box');
     arrow.classList.toggle('rotate');
 }
+
+/*
+// FadeIn cover hover animation bugs (if only use css)
+// select all .box element
+const boxes = document.querySelectorAll('.box');
+for (let i = 0; i < boxes.length; i++) {
+    const box = boxes[i];
+    box.classList.add('fadeIn_animation'); // add FadeIn to box
+    
+    const delay = i * 50;
+    box.style.animationDelay = delay + 'ms';
+
+    // if hover
+    box.addEventListener('mouseover', function() { 
+        // remove fadeIn animation, add transform transition
+        this.classList.remove('leaveHover_animation');
+        this.classList.remove('fadeIn_animation');
+        this.classList.add('hover_animation');
+    });
+    
+    // if leave hover
+    box.addEventListener('mouseleave', function() {
+        // add animation
+        this.classList.remove('hover_animation');
+        this.classList.add('leaveHover_animation');
+    });
+}
+*/
+
